95e481d69e340e35ccc968bc30db3e7f3307b93f
Kjetil Grun <kjetil.grun@nina.no>
2025-09-03 13:18:13 +0200

    Removed unnecessary lines and the one with install_github that created an error

--- app.R
+++ app.R
@@ -1,10 +1,3 @@
 #app.R
-# Reload HarvestGolem with this code to get it in to the shiny libraries
-#library(withr)
-#library(devtools)
-#with_libpaths(new = "/data/shiny/packages/", install_github("DrMattG/HarvestGolem", force=TRUE))
-#with_libpaths(new = "/home/NINA.NO/matthew.grainger/R/x86_64-pc-linux-gnu-library/4.0", install_github("DrMattG/HarvestGolem", force=TRUE))
-
-install_github("DrMattG/HarvestGolem", force=TRUE)
 library(HarvestGolem)
 HarvestGolem::run_app()
